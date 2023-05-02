b74800a48e00cc19f5f05a3e65c0b9173f55ca2e
EnriFermi <aht.misha@yandex.ru>
2023-05-02 14:49:29 +0300

    m_check interface added

---
+++ run.sh
@@ -0,0 +1,6 @@
+EXEC_O_PATH="exec/o"
+EXEC_PATH="exec"
+g++ -c -o $EXEC_O_PATH/grader.o -std=c++17 -O3 src/grader.cpp
+g++ -c -o $EXEC_O_PATH/solution.o -std=c++17 -O3 src/solution.cpp
+g++ -o $EXEC_PATH/solution -std=c++17 -O3 src/edge_check/edge_check.cpp $EXEC_O_PATH/grader.o $EXEC_O_PATH/solution.o
+./$EXEC_PATH/solution > out.txt
---
+++ src/edge_check/edge_check.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct VertexInfo;
+
+class Edge_checker {
+    const int N;
+    const int M;
+    const int L;
+    Edge_checker(int N, int M, int L, vector<VertexInfo> &std_struct): N(N), M(M), L(L){
+        // vector<VertexInfo>::const_iterator i = std_struct.cbegin();
+    };
+    
+    bool check_edge(char * wurst){
+
+    }
+    ~Edge_checker(){};
+
+};

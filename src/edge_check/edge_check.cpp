f0f9228ebdbce06411246fd2a06ecea65009d66c
EnriFermi <aht.misha@yandex.ru>
2023-05-02 17:35:58 +0300

    fff

--- src/edge_check/edge_check.cpp
+++ src/edge_check/edge_check.cpp
@@ -9,8 +9,11 @@ class Edge_checker {
     const int N;
     const int M;
     const int L;
-    Edge_checker(int N, int M, int L, vector<VertexInfo> &std_struct): N(N), M(M), L(L){
-        // vector<VertexInfo>::const_iterator i = std_struct.cbegin();
+    Edge_checker(int N, int M, int L, const vector<VertexInfo> &std_struct): N(N), M(M), L(L){
+        vector<VertexInfo>::const_iterator i = std_struct.cbegin(), end = std_struct.cend();
+        while(i!= end){
+            
+        }
     };
     
     bool check_edge(char * wurst){

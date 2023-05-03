2bfd84a33698a2e8eb3263f7071a91bd7189f466
MarkJulian19 <ktyf351@mail.ru>
2023-05-03 19:51:10 +0300

    m_check5

--- src/m_check/m_check.cpp
+++ src/m_check/m_check.cpp
@@ -13,118 +13,129 @@ struct VertexInfo
     std::vector<int> primaryEdges, secondaryEdges = {};
 };
 
-// std::vector<std::vector<int>> Solver(int N, int M, int L, std::vector<VertexInfo> infos);
-
 std::vector<VertexInfo> infos;
 
 class m_check
 {
     int N, M, L;
     // int *W_arr = NULL;
-    int *L_P_arr = NULL;
-    int *L_S_arr = NULL;
+    char *L_P_arr = NULL;
+    char *L_S_arr = NULL;
     char *WEIGHT_C = NULL;
     char *INDEX_C = NULL;
 
 public:
-    m_check(int n, int m, int l, std::vector<VertexInfo> &std_struct, char *index_c, char *weight_c) : N(n), M(m), L(l)
+    m_check(int n, int m, int l, std::vector<VertexInfo> &std_struct, char *index_c, char *weight_c) : N(n), M(m), L(l), WEIGHT_C(weight_c), INDEX_C(index_c)
     {
-        for (int i = 0; i < N; i++)
-        {
-            std::cout << (int)weight_c[i] << ' ';
-        }
-        std::cout << std::endl;
         WEIGHT_C = weight_c;
         INDEX_C = index_c;
-        // W_arr = new int[N];
-        // std::cout << W_arr[0] << std::endl;
-        // L_P_arr = new int *[L]; // инициализация массива массивов P вершин на каждом уровне
-        // for (int i = 0; i < L; i++)
-        L_P_arr = new int[N];
-        // for (int i = 0; i < L; i++)
-        //     for (int j = 0; j < N; j++)
-        //         L_P_arr[i][j] = 0;
-        // memset(L_P_arr[i], 0, N * sizeof(int));
-        // L_S_arr = new int *[L]; // инициализация массива массивов S вершин на каждом уровне
-        // for (int i = 0; i < L; i++)
-        L_S_arr = new int[N];
-        // for (int i = 0; i < L; i++)
-        //     for (int j = 0; j < N; j++)
-        //         L_S_arr[i][j] = 0;
-        // memset(L_S_arr[i], 0, N * sizeof(int));
+        L_P_arr = (char *)malloc(N * 8);
+        L_S_arr = (char *)malloc(N * 8);
         for (int i = 0; i < N; i++) // могут вылезти ошибки ввиду архитектуры, надеемся что праймари всегда существует а секондари при отсутствии имеет уровень -1
         {
-            // W_arr[i] = std_struct[i].weight;
             L_P_arr[i] = std_struct[index_c[i]].primaryLvl;
-            // if (std_struct[index_c[i]].secondaryLvl >= 0)
             L_S_arr[i] = std_struct[index_c[i]].secondaryLvl;
         }
-
-        // std::cout << "m_check()" << std::endl;
     }
     ~m_check()
     {
-        // delete[] W_arr;
-        // for (int i = 0; i < L; i++)
-        // for (int i = 0; i < L; i++)
-        delete[] L_S_arr;
-        delete[] L_P_arr;
-        // std::cout << "~m_check()" << std::endl;
+        free(L_S_arr);
+        free(L_P_arr);
     }
 
     void get_info()
     {
         std::cout << N << ' ' << M << ' ' << L << std::endl;
-        // for (int i = 0; i < N; i++)
-        // {
-        //     std::cout << W_arr[i] << ' ';
-        // }
-        // std::cout << std::endl;
-        // std::cout << std::endl;
-        // for (int i = 0; i < L; i++)
-        // {
-        //     for (int j = 0; j < N; j++)
-        //         std::cout << L_P_arr[i][j] << ' ';
-        //     std::cout << std::endl;
-        // }
-        // std::cout << std::endl;
-        // for (int i = 0; i < L; i++)
-        // {
-        //     for (int j = 0; j < N; j++)
-        //         std::cout << L_S_arr[i][j] << ' ';
-        //     std::cout << std::endl;
-        // }
         for (int i = 0; i < N; i++)
         {
-            std::cout << L_P_arr[i] << ' ';
+            std::cout << (int)L_P_arr[i] << ' ';
         }
         std::cout << std::endl;
         for (int i = 0; i < N; i++)
         {
-            std::cout << L_S_arr[i] << ' ';
+            std::cout << (int)L_S_arr[i] << ' ';
         }
         std::cout << std::endl;
     }
+
     bool is_good(char **arr, char K)
     {
-        char tmp_arr[K][L] = {0};
-        for (int i = 0; i < K; i++)
+        char tmp_arr[K][L]; //! не забыть оптимизировать
+        memset(tmp_arr, 0, K * L);
+        for (int i = 0; i <= K; i++)
         {
-            if ((arr[0][i] > 0) && (L_P_arr[i] != -1))
+            char a = arr[0][i];
+            char c = L_P_arr[i];
+            if ((a > 0)) // && (L_P_arr[i] != -1))
             {
-                tmp_arr[arr[0][i]][L_P_arr[i]] += WEIGHT_C[i];
-                if (tmp_arr[arr[0][i]][L_P_arr[i]] > M)
+                tmp_arr[a][c] += WEIGHT_C[i];
+                if (tmp_arr[a][c] > M)
                 {
                     return false;
                 }
             }
-            if ((arr[1][i] > 0) && (L_S_arr[i] != -1))
+            char b = arr[1][i];
+            char d = L_S_arr[i];
+            if ((b > 0) && (d != -1))
             {
-                tmp_arr[arr[1][i]][L_S_arr[i]] += WEIGHT_C[i];
-                if (tmp_arr[arr[1][i]][L_S_arr[i]] > M)
+                tmp_arr[b][d] += WEIGHT_C[i];
+                if (tmp_arr[b][d] > M)
                 {
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+
+    bool is_good_fast(char **arr, char K)
+    {
+        char last_p_grup = arr[0][K];
+        char last_s_grup = arr[1][K];
+        if ((last_p_grup == last_s_grup) && (last_p_grup != 0))
+        {
+            char tmp_arr[L];
+            memset(tmp_arr, 0, L);
+            while (K >= 0)
+            {
+                if (arr[0][K] == last_p_grup)
+                {
+                    char b = L_P_arr[K];
+                    tmp_arr[b] += WEIGHT_C[K];
+                    if (tmp_arr[b] > M)
+                    {
+                        return false;
+                    }
+                }
+                if (arr[1][K] == last_s_grup)
+                {
+                    char b = L_S_arr[K];
+                    tmp_arr[b] += WEIGHT_C[K];
+                    if (tmp_arr[b] > M)
+                    {
+                        return false;
+                    }
+                }
+                K--;
+            }
+            return true;
+        }
+        if ((last_s_grup == 0) && (last_p_grup != last_s_grup))
+        {
+            char tmp_arr[L];
+            memset(tmp_arr, 0, L);
+            while (K >= 0)
+            {
+                if (arr[0][K] == last_p_grup)
+                {
+                    char b = L_P_arr[K];
+                    tmp_arr[b] += WEIGHT_C[K];
+                    if (tmp_arr[b] > M)
+                    {
+                        return false;
+                    }
+                }
+                K--;
             }
         }
         return true;
@@ -160,26 +171,22 @@ int main()
         }
     }
 
-    char weight_c[N] = {7, 4, 4, 4, 4};
-    char index_c[N] = {2, 1, 0, 3, 4};
+    char weight_c[N] = {4, 4, 7, 4, 4};
+    char index_c[N] = {0, 1, 2, 3, 4};
     char **num_mas = new char *[2];
-    num_mas[0] = new char[5]{1, 0, 3, 3, 0};
-    num_mas[1] = new char[5]{3, 2, 2, 0, 0};
-    // unsigned int start_time = clock();
-    // for (int i = 0; i < 100000; i++)
-    // {
-    //     m_check a1 = m_check(N, M, L, convertedInfos, index_c, weight_c);
-    // }
-    unsigned int start_time = clock();
+    num_mas[0] = new char[5]{0, 0, 1, 3, 3};
+    num_mas[1] = new char[5]{2, 2, 0, 0, 3};
     m_check a1 = m_check(N, M, L, convertedInfos, index_c, weight_c);
-    unsigned int end_time = clock();
     a1.get_info();
-    start_time = clock();
-    for (int i = 0; i < 2000; i++)
+    unsigned int start_time = clock();
+    for (int i = 0; i < 40000000; i++)
     {
-        a1.is_good(num_mas, 5);
+        a1.is_good(num_mas, 4);
+        // m_check a1 = m_check(N, M, L, convertedInfos, index_c, weight_c);
+        // std::cout << a1.is_good_fast(num_mas, 4) << std::endl;
+        // a1.is_good_fast(num_mas, 4);
     }
-    end_time = clock();
+    unsigned int end_time = clock();
     std::cout << 1000.0 * (end_time - start_time) / CLOCKS_PER_SEC << std::endl;
     delete[] num_mas[0];
     delete[] num_mas[1];

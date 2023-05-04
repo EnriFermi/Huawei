29e4107cbcbb8379e11cc6912d9557b69b02b8bd
MarkJulian19 <ktyf351@mail.ru>
2023-05-04 22:00:50 +0300

    m_check6

--- src/m_check/m_check.cpp
+++ src/m_check/m_check.cpp
@@ -1,3 +1,6 @@
+// Created by Nikita Kyazhin 03.05.23
+// m_check
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -62,134 +65,187 @@ public:
     {
         char tmp_arr[K][L]; //! не забыть оптимизировать
         memset(tmp_arr, 0, K * L);
-        for (int i = 0; i <= K; i++)
+        for (int i = K; i < N; i++) // от к-го элемента по самый конец
         {
             char a = arr[0][i];
+            if (a == 0)
+                continue;
             char c = L_P_arr[i];
-            if ((a > 0)) // && (L_P_arr[i] != -1))
+            // if ((a > 0)) // && (L_P_arr[i] != -1))
+            // {
+            tmp_arr[a][c] += WEIGHT_C[i];
+            if (tmp_arr[a][c] > M)
             {
-                tmp_arr[a][c] += WEIGHT_C[i];
-                if (tmp_arr[a][c] > M)
-                {
-                    return false;
-                }
+                return false;
             }
+            // }
             char b = arr[1][i];
+            if (b == 0)
+                continue;
             char d = L_S_arr[i];
-            if ((b > 0) && (d != -1))
+            // if ((b > 0) && (d != -1))
+            // {
+            tmp_arr[b][d] += WEIGHT_C[i];
+            if (tmp_arr[b][d] > M)
             {
-                tmp_arr[b][d] += WEIGHT_C[i];
-                if (tmp_arr[b][d] > M)
-                {
-                    return false;
-                }
+                return false;
             }
+            // }
         }
         return true;
     }
 
     bool is_good_fast(char **arr, char K)
     {
-        char last_p_grup = arr[0][K];
-        char last_s_grup = arr[1][K];
-        if ((last_p_grup == last_s_grup) && (last_p_grup != 0))
+        char first_p_grup = arr[0][K];
+        // if (first_p_grup == 0)
+        //     return true;
+        char first_s_grup = arr[1][K];
+        if (first_s_grup == 0)
         {
             char tmp_arr[L];
             memset(tmp_arr, 0, L);
-            while (K >= 0)
+            for (int i = K; i < N; i++)
             {
-                if (arr[0][K] == last_p_grup)
+                if (arr[0][i] == first_p_grup)
                 {
-                    char b = L_P_arr[K];
-                    tmp_arr[b] += WEIGHT_C[K];
+                    char b = L_P_arr[i];
+                    tmp_arr[b] += WEIGHT_C[i];
                     if (tmp_arr[b] > M)
                     {
                         return false;
                     }
                 }
-                if (arr[1][K] == last_s_grup)
-                {
-                    char b = L_S_arr[K];
-                    tmp_arr[b] += WEIGHT_C[K];
-                    if (tmp_arr[b] > M)
-                    {
-                        return false;
-                    }
-                }
-                K--;
             }
             return true;
         }
-        if ((last_s_grup == 0) && (last_p_grup != last_s_grup))
+        else
         {
             char tmp_arr[L];
             memset(tmp_arr, 0, L);
-            while (K >= 0)
+            for (int i = K; i < N; i++)
             {
-                if (arr[0][K] == last_p_grup)
+                if (arr[0][i] == first_p_grup)
                 {
-                    char b = L_P_arr[K];
-                    tmp_arr[b] += WEIGHT_C[K];
+                    char b = L_P_arr[i];
+                    tmp_arr[b] += WEIGHT_C[i];
                     if (tmp_arr[b] > M)
                     {
                         return false;
                     }
+
+                    if (arr[1][i] == first_p_grup)
+                    {
+                        char b = L_S_arr[i];
+                        tmp_arr[b] += WEIGHT_C[i];
+                        if (tmp_arr[b] > M)
+                        {
+                            return false;
+                        }
+                    }
                 }
-                K--;
             }
+            return true;
         }
-        return true;
     }
 };
+//     if ((first_p_grup == first_s_grup) && (first_p_grup != 0))
+//     {
+//         char tmp_arr[L];
+//         memset(tmp_arr, 0, L);
+//         while (K >= 0)
+//         {
+//             if (arr[0][K] == first_p_grup)
+//             {
+//                 char b = L_P_arr[K];
+//                 tmp_arr[b] += WEIGHT_C[K];
+//                 if (tmp_arr[b] > M)
+//                 {
+//                     return false;
+//                 }
+//             }
+//             if (arr[1][K] == first_s_grup)
+//             {
+//                 char b = L_S_arr[K];
+//                 tmp_arr[b] += WEIGHT_C[K];
+//                 if (tmp_arr[b] > M)
+//                 {
+//                     return false;
+//                 }
+//             }
+//             K--;
+//         }
+//         return true;
+//     }
+//     if ((first_s_grup == 0) && (first_p_grup != first_s_grup))
+//     {
+//         char tmp_arr[L];
+//         memset(tmp_arr, 0, L);
+//         while (K >= 0)
+//         {
+//             if (arr[0][K] == first_p_grup)
+//             {
+//                 char b = L_P_arr[K];
+//                 tmp_arr[b] += WEIGHT_C[K];
+//                 if (tmp_arr[b] > M)
+//                 {
+//                     return false;
+//                 }
+//             }
+//             K--;
+//         }
+//     }
+//     return true;
+// }
 
-int main()
-{
+// int main()
+// {
 
-    int N = 5, M = 10, L = 3;
-    infos = {VertexInfo{/*weight=*/4, /*lvlsCount=*/2, /*primaryLvl=*/1, /*secondaryLvl=*/0,
-                        /*primaryEdges=*/{0, 0, 0, 0, 2}, /*secondaryEdges=*/{0, 1, 0, 0, 0}},
-             VertexInfo{/*weight=*/4, /*lvlsCount=*/2, /*primaryLvl=*/2, /*secondaryLvl=*/0,
-                        /*primaryEdges=*/{0, 0, 0, 0, 0}, /*secondaryEdges=*/{1, 0, 0, 0, 0}},
-             VertexInfo{/*weight=*/7, /*lvlsCount=*/2, /*primaryLvl=*/0, /*secondaryLvl=*/2,
-                        /*primaryEdges=*/{0, 0, 0, 0, 0}, /*secondaryEdges=*/{0, 0, 0, 0, 3}},
-             VertexInfo{/*weight=*/4, /*lvlsCount=*/1, /*primaryLvl=*/1, /*secondaryLvl=*/-1,
-                        /*primaryEdges=*/{0, 0, 0, 0, 3}, /*secondaryEdges=*/{}},
-             VertexInfo{/*weight=*/4, /*lvlsCount=*/2, /*primaryLvl=*/1, /*secondaryLvl=*/2,
-                        /*primaryEdges=*/{2, 0, 0, 3, 0}, /*secondaryEdges=*/{0, 0, 3, 0, 0}}};
-    std::vector<VertexInfo> convertedInfos = infos;
-    for (int v = 0; v < N; ++v)
-    {
-        for (int &e : convertedInfos[v].primaryEdges)
-        {
-            if (e >= 2)
-                e = -1;
-        }
-        for (int &e : convertedInfos[v].secondaryEdges)
-        {
-            if (e >= 2)
-                e = -1;
-        }
-    }
+//     int N = 5, M = 10, L = 3;
+//     infos = {VertexInfo{/*weight=*/4, /*lvlsCount=*/2, /*primaryLvl=*/1, /*secondaryLvl=*/0,
+//                         /*primaryEdges=*/{0, 0, 0, 0, 2}, /*secondaryEdges=*/{0, 1, 0, 0, 0}},
+//              VertexInfo{/*weight=*/4, /*lvlsCount=*/2, /*primaryLvl=*/2, /*secondaryLvl=*/0,
+//                         /*primaryEdges=*/{0, 0, 0, 0, 0}, /*secondaryEdges=*/{1, 0, 0, 0, 0}},
+//              VertexInfo{/*weight=*/7, /*lvlsCount=*/2, /*primaryLvl=*/0, /*secondaryLvl=*/2,
+//                         /*primaryEdges=*/{0, 0, 0, 0, 0}, /*secondaryEdges=*/{0, 0, 0, 0, 3}},
+//              VertexInfo{/*weight=*/4, /*lvlsCount=*/1, /*primaryLvl=*/1, /*secondaryLvl=*/-1,
+//                         /*primaryEdges=*/{0, 0, 0, 0, 3}, /*secondaryEdges=*/{}},
+//              VertexInfo{/*weight=*/4, /*lvlsCount=*/2, /*primaryLvl=*/1, /*secondaryLvl=*/2,
+//                         /*primaryEdges=*/{2, 0, 0, 3, 0}, /*secondaryEdges=*/{0, 0, 3, 0, 0}}};
+//     std::vector<VertexInfo> convertedInfos = infos;
+//     for (int v = 0; v < N; ++v)
+//     {
+//         for (int &e : convertedInfos[v].primaryEdges)
+//         {
+//             if (e >= 2)
+//                 e = -1;
+//         }
+//         for (int &e : convertedInfos[v].secondaryEdges)
+//         {
+//             if (e >= 2)
+//                 e = -1;
+//         }
+//     }
 
-    char weight_c[N] = {4, 4, 7, 4, 4};
-    char index_c[N] = {0, 1, 2, 3, 4};
-    char **num_mas = new char *[2];
-    num_mas[0] = new char[5]{0, 0, 1, 3, 3};
-    num_mas[1] = new char[5]{2, 2, 0, 0, 3};
-    m_check a1 = m_check(N, M, L, convertedInfos, index_c, weight_c);
-    a1.get_info();
-    unsigned int start_time = clock();
-    for (int i = 0; i < 40000000; i++)
-    {
-        a1.is_good(num_mas, 4);
-        // m_check a1 = m_check(N, M, L, convertedInfos, index_c, weight_c);
-        // std::cout << a1.is_good_fast(num_mas, 4) << std::endl;
-        // a1.is_good_fast(num_mas, 4);
-    }
-    unsigned int end_time = clock();
-    std::cout << 1000.0 * (end_time - start_time) / CLOCKS_PER_SEC << std::endl;
-    delete[] num_mas[0];
-    delete[] num_mas[1];
-    delete[] num_mas;
-    return 0;
-}
+//     char weight_c[N] = {4, 4, 7, 4, 4};
+//     char index_c[N] = {0, 1, 2, 3, 4};
+//     char **num_mas = new char *[2];
+//     num_mas[0] = new char[5]{2, 0, 1, 3, 3};
+//     num_mas[1] = new char[5]{2, 2, 2, 0, 3};
+//     m_check a1 = m_check(N, M, L, convertedInfos, index_c, weight_c);
+//     a1.get_info();
+//     unsigned int start_time = clock();
+//     for (int i = 0; i < 2000000; i++)
+//     {
+//         // a1.is_good(num_mas, 0);
+//         // m_check a1 = m_check(N, M, L, convertedInfos, index_c, weight_c);
+//         // std::cout << a1.is_good_fast(num_mas, 4) << std::endl;
+//         // a1.is_good_fast(num_mas, 0);
+//     }
+//     unsigned int end_time = clock();
+//     std::cout << 1000.0 * (end_time - start_time) / CLOCKS_PER_SEC << std::endl;
+//     delete[] num_mas[0];
+//     delete[] num_mas[1];
+//     delete[] num_mas;
+//     return 0;
+// }
--- src/solution.cpp
+++ src/solution.cpp
@@ -1,25 +1,104 @@
 #include <cassert>
 #include <vector>
 #include <algorithm>
-#include "m_check/m_check.cpp"
-#include "edge_check/edge_check.cpp"
-#include "generator/generator.cpp"
-bool Delete(int/* lvl*/, int/* v*/, int/* u*/);
+#include <iostream>
+// #include "m_check/m_check.cpp"
+// #include "edge_check/edge_check.cpp"
+// #include "generator/generator.cpp"
+bool Delete(int /* lvl*/, int /* v*/, int /* u*/);
 
-std::vector<std::vector<int>> Solver(int N, int M, int L, std::vector<VertexInfo>&  infos) {
-    //Preprocessing
-    //TODO sort
-    
-    char * index_c;
-    char * weight_c;
-    //Checkers initialising 
-    m_check m_checker(N, M, L, infos, index_c, weight_c);
-    edge_check edge_checker(N, M, L, infos);
+struct VertexInfo
+{
+    int weight;
+    int lvlsCount;
+    int primaryLvl, secondaryLvl = -1;
+    std::vector<int> primaryEdges, secondaryEdges = {};
+};
+
+std::vector<VertexInfo> infos;
+
+/*std::vector<std::vector<int>>*/ void Solver(int N, int M, int L, std::vector<VertexInfo> &infos)
+{
+    // Preprocessing
+    // TODO sort
+
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << infos[i].weight << ' ';
+    }
+    std::cout << std::endl;
+    char index_c[N];
+    for (int i = 0; i < N; i++)
+        index_c[i] = i;
+    bool comp(const VertexInfo &x, const VertexInfo &y){
+        return x.weight < y.weight;
+    };
 
 
-    //Generation
-    //TODO change sort output(maybe postprocessing)
-    generator().generate();
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << infos[i].weight << ' ';
+    }
+
+    char *weight_c;
+    // Checkers initialising
+    //! m_check m_checker(N, M, L, infos, index_c, weight_c);
+    //! edge_check edge_checker(N, M, L, infos);
+
+    // Generation
+    // TODO change sort output(maybe postprocessing)
+    //! generator().generate();
 
     return;
+}
+int main()
+{
+
+    int N = 5, M = 10, L = 3;
+    infos = {VertexInfo{/*weight=*/4, /*lvlsCount=*/2, /*primaryLvl=*/1, /*secondaryLvl=*/0,
+                        /*primaryEdges=*/{0, 0, 0, 0, 2}, /*secondaryEdges=*/{0, 1, 0, 0, 0}},
+             VertexInfo{/*weight=*/4, /*lvlsCount=*/2, /*primaryLvl=*/2, /*secondaryLvl=*/0,
+                        /*primaryEdges=*/{0, 0, 0, 0, 0}, /*secondaryEdges=*/{1, 0, 0, 0, 0}},
+             VertexInfo{/*weight=*/7, /*lvlsCount=*/2, /*primaryLvl=*/0, /*secondaryLvl=*/2,
+                        /*primaryEdges=*/{0, 0, 0, 0, 0}, /*secondaryEdges=*/{0, 0, 0, 0, 3}},
+             VertexInfo{/*weight=*/4, /*lvlsCount=*/1, /*primaryLvl=*/1, /*secondaryLvl=*/-1,
+                        /*primaryEdges=*/{0, 0, 0, 0, 3}, /*secondaryEdges=*/{}},
+             VertexInfo{/*weight=*/4, /*lvlsCount=*/2, /*primaryLvl=*/1, /*secondaryLvl=*/2,
+                        /*primaryEdges=*/{2, 0, 0, 3, 0}, /*secondaryEdges=*/{0, 0, 3, 0, 0}}};
+    std::vector<VertexInfo> convertedInfos = infos;
+    for (int v = 0; v < N; ++v)
+    {
+        for (int &e : convertedInfos[v].primaryEdges)
+        {
+            if (e >= 2)
+                e = -1;
+        }
+        for (int &e : convertedInfos[v].secondaryEdges)
+        {
+            if (e >= 2)
+                e = -1;
+        }
+    }
+    Solver(N, M, L, convertedInfos);
+    // char weight_c[N] = {4, 4, 7, 4, 4};
+    // char index_c[N] = {0, 1, 2, 3, 4};
+    // char **num_mas = new char *[2];
+    // num_mas[0] = new char[5]{2, 0, 1, 3, 3};
+    // num_mas[1] = new char[5]{2, 2, 2, 0, 3};
+    // m_check a1 = m_check(N, M, L, convertedInfos, index_c, weight_c);
+    // a1.get_info();
+    // unsigned int start_time = clock();
+    // for (int i = 0; i < 2000000; i++)
+    // {
+    //     // a1.is_good(num_mas, 0);
+    //     // m_check a1 = m_check(N, M, L, convertedInfos, index_c, weight_c);
+    //     // std::cout << a1.is_good_fast(num_mas, 4) << std::endl;
+    //     // a1.is_good_fast(num_mas, 0);
+    // }
+    // unsigned int end_time = clock();
+    // std::cout << 1000.0 * (end_time - start_time) / CLOCKS_PER_SEC << std::endl;
+    // delete[] num_mas[0];
+    // delete[] num_mas[1];
+    // delete[] num_mas;
+    return 0;
 }

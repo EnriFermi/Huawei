6cd50a73bfa9699bf7e5ad3038173d5d088de2e9
SannTim <timohaj1@yandex.ru>
2023-05-06 13:02:51 +0300

    degenerator

--- src/generator/generator.cpp
+++ src/generator/generator.cpp
@@ -4,10 +4,11 @@
 //
 //  Created by Тимофей Санников on 02.05.2023.
 //
-
+//TODO accuracy
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
+#include <vector>
 
 //TODO сделать accuracy неконстантой
 #define accuracy 2
@@ -35,16 +36,19 @@ void print_mas(char ** mas, int a, int b){
 class generator{
 //    generator(){}
 public:
-    char t;
-    char k;
-    char M;
-    char pmax;
-    char * W_arr;
-    int Summall_p;
-    bool *sec_ed;
+    char t; // кол-во нулей в маске
+    char k; // n в начальной программе, хз захотелось юзать k
+    char M; // M в начальной программе
+    char coinlost = 0; // кол-во потраченных монет
+    char * W_arr; // массив весов
+    short Summall_p; // сумма всех primary
+    bool *sec_ed; // существует ли secondary
+    char unsttv;
+    int W_now, W_next;
+    std::vector<char***> st;
     
-    bool done_flag;
-    char ** ans;
+    bool done_flag; // флаг конца обработки
+    char ** ans; //массив ответов
     //создает копию массива 2хk
     char * create_mass_copy(char* masi){
         char * newmasi = new char[k];
@@ -60,35 +64,59 @@ public:
         return newmasi;
     }
     
+    void count_weight(char ** masi, char w){
+        int all_w = 0;
+        for (char i = 0; i < k; i++){
+            if (masi[i][0] != 1){
+                if (masi[i][1] != 1) all_w += W_arr[i];
+                all_w += W_arr[i];
+            }
+        }
+        W_now = all_w;
+        W_next = all_w - w;
+    }
+    
     // создание масок
-    void maska_requr_prime(char now, char used, char** masi){
+    void maska_requr_prime(char now, char used, char** masi, bool bruh = false){
         if (done_flag) return;
         if (t - used <= now+1) return; // t - used: сколько осталось
         if (used == t){
-                char ** num_mas = new char * [2];
-                num_mas[0] = new char[k]{0};
-                num_mas[1] = new char[k]{0};
-                megarecur(masi, num_mas, k-1, k-t, 1);
-                delete[] num_mas[0];
-                delete[] num_mas[1];
-                delete[] num_mas;
+            char ** num_mas = new char * [2];
+            num_mas[0] = new char[k]{0};
+            num_mas[1] = new char[k]{0};
+            megarecur(masi, num_mas, k-1, k-t, 1);
+            if (st.size() != 0) {
+                if (bruh)
+                    count_weight(masi, W_arr[now]);
+                else{
+                    if (now != k-1)
+                        count_weight(masi, W_arr[now+1]);
+                    else count_weight(masi, M);
+                }
+                finito_la_comedia();
+            }
+            delete[] num_mas[0];
+            delete[] num_mas[1];
+            delete[] num_mas;
+            if(done_flag) return;
         }
         
         char ** ne_masi = create_mass_copy(masi);
         // 00
-        maska_requr_prime(now--, used, ne_masi);
+        if (sec_ed[now]){
+            maska_requr_prime(now-1, used, ne_masi, true);
+        }
         ne_masi[1][now] = 1; // 01
-        maska_requr_prime(now--, used, ne_masi);
+        maska_requr_prime(now-1, used, ne_masi);
         ne_masi[0][now] = 1; // 11
-        maska_requr_prime(now--, used+1, ne_masi);
+        maska_requr_prime(now-1, used+1,ne_masi);
         delete[] ne_masi[0];
         delete[] ne_masi[1];
         delete[] ne_masi;
     }
-    bool valid(char num_of_del){
-        
-        return true;
-    }
+    
+    
+    
     bool good_del(char num_of_del, char ** del_node){
         bool t = true;
         char iter = num_of_del;
@@ -96,16 +124,57 @@ public:
             t = t && Delete(del_node[iter][0],del_node[iter][1],del_node[iter][2]);
             iter --;
         }
+        if (!t) coinlost++;
         return t;
     }
+    
+    void hurray(char ** a){
+        ans = create_mass_copy(a);
+        done_flag = true;
+        return;
+    }
+    inline static double count_decision_value(int w_sum, int w_chgsum, int u_num, int c_num, double variance, int u_mnum);
     void make_max(char ** a, char num_of_del, char ** del_node){
         // добавить delete проверку
-        if (valid(num_of_del)){
-            if (good_del(num_of_del, del_node)){
-                done_flag = true;
-                ans = create_mass_copy(a);
+        if (num_of_del == 0) hurray(a);
+        else {
+            char *** np = new char**[3];
+            np[0] = a;
+            np[1] = new char*[1];
+            np[1][0] = new char[1];
+            np[1][0] = &num_of_del; // np[1][0][0] = num_of_del;
+            np[2] = del_node;
+            st.push_back(np);
+        }
+    }
+    void finito_la_comedia(){
+        
+        char imax = 0;
+        double maxo = -1, nw;
+        for (char i = 0; i < st.size(); i++){
+            if (maxo < (nw = count_decision_value(W_now, W_next, st[i][1][0][0], coinlost, zv, unsttv))){
+                maxo = nw;
+                imax = i;
             }
         }
+        if (maxo > 0.5)
+            if (good_del(st[imax][1][0][0], st[imax][2])){
+                hurray(st[imax][0]);
+            }
+        
+        for (char i = 0; i < st.size(); i++){
+            //добавить удаление строки массива
+            delete[] st[i][0][0];
+            delete[] st[i][0][1];
+            for (char j = 0; j < st[i][1][0][0]; j++)
+                delete [] st[i][2][j];
+            delete[] st[i][1][0];
+            delete[] st[i][0];
+            delete[] st[i][1];
+            delete[] st[i][2];
+            delete[] st[i];
+        }
+        st.clear();
     }
     
     
@@ -158,11 +227,13 @@ public:
                         
                         if (done_flag) return;
                         
-                        if (j + accuracy >= k){
+                        if (j + accuracy >= k && num_of_dell == 0){
                             make_max(new_masi, num_of_dell, del_node);
                         }
                         else{
-                            //добавить удаление new_del_node + в конце концов добавить функцию для accuracy
+                            for (char i = 0; i < num_of_dell+del_add;i++)
+                                delete[] new_del_node[i];
+                            
                             delete[] new_masi[0];
                             delete[] new_masi[1];
                             delete[] new_masi;
